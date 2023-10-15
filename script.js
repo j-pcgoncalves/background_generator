c41de644fc8115240e5eff04835779d297729ac3
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-10-15 22:57:54 +0100

    refactor code

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const color_1 = document.getElementById("color_1");
 const color_2 = document.getElementById("color_2");
 const body = document.getElementById("gradient_bg");
 
-color_1.addEventListener("input", () => {
+function setGradient() {
     body.style.background = `linear-gradient(to right, ${color_1.value}, ${color_2.value})`;
+}
+
+color_1.addEventListener("input", () => {
+    setGradient();
 });
 
 color_2.addEventListener("input", () => {
-    body.style.background = `linear-gradient(to right, ${color_1.value}, ${color_2.value})`;
+    setGradient();
 });
